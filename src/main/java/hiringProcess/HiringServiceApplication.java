ce50aa9239609d1a8072d17399527395ab4a5e92
fkvn <phucaone@gmail.com>
2020-01-31 13:34:59 -0800

    Enabled Swagger2 and CORS

--- src/main/java/hiringProcess/HiringServiceApplication.java
+++ src/main/java/hiringProcess/HiringServiceApplication.java
@@ -1,16 +1,38 @@
 package hiringProcess;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
-@Slf4j
+@EnableSwagger2
 public class HiringServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(HiringServiceApplication.class, args);
-    log.warn("starting log");
+  }
+  
+  @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+  
+  @Bean
+  public Docket productApi() {
+     return new Docket(DocumentationType.SWAGGER_2).select()
+        .apis(RequestHandlerSelectors.basePackage("hiringProcess.web.controller")).build();
   }
 }
